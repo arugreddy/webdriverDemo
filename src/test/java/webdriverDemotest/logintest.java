de3e91dad10622a07b0b1f585b438681b1aed58c
arugreddy <68020898+arugreddy@users.noreply.github.com>
2020-07-10 18:10:31 -0400

    Webdriver demo code is added

---
+++ src/test/java/webdriverDemotest/logintest.java
@@ -0,0 +1,37 @@
+package webdriverDemotest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterSuite;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class logintest {
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\AR\\Drivers\\ChromeDriver.exe");
+		 driver = new ChromeDriver();
+		 driver.get("https://opensource-demo.orangehrmlive.com");
+	}
+
+	@Test
+	public void logintest() {
+		//driver.get("https://opensource-demo.orangehrmlive.com");
+		//String titleString = driver.getTitle();
+         driver.findElement(By.name("txtUsername")).sendKeys("Admin");
+         driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+         driver.findElement(By.name("Submit")).click();
+		//System.out.println(titleString);
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
